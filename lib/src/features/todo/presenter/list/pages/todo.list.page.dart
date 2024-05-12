ddb55ba79bb57d061fcfeb62b6e60e09bca2216e
Marcelo Block Teixeira <marcelochb@gmail.com>
2024-05-12 00:36:15 -0300

    riverpod and getIt

--- lib/src/features/todo/presenter/list/pages/todo.list.page.dart
+++ lib/src/features/todo/presenter/list/pages/todo.list.page.dart
@@ -1,24 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../controlers/todo.list.controller.dart';
 
 class TodoListPage extends ConsumerWidget {
-  const TodoListPage({
-    super.key,
-    });
-
-
+  const TodoListPage({super.key});
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final todoList = ref.watch(todoListControllerProvider);
+    final provider = ref.watch(todoListControllerProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Todo List'),
       ),
       body: Center(
-        child: switch (todoList) {
+        child: switch (provider) {
           AsyncData(:final value) => ListView.builder(
             itemCount: value.length,
             itemBuilder: (context, index) {
